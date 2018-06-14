f28bea02eb9c06ca447de8223e9f7659e5100a5d
ankitsejwal <ankit@sejw.al>
2018-06-15 03:20:58 +1000

    sending text from twilio no

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
-from flask import Flask, render_template
+import os
+from twilio.rest import Client
 
-app = Flask(__name__)
+account_sid     = os.environ["TWILIO_ACCOUNT_SID"]
+auth_token      = os.environ["TWILIO_AUTH_TOKEN"]
+my_number       = os.environ["MY_NUMBER"]
+twilio_number   = os.environ["TWILIO_NUMBER"]
 
-@app.route('/')
-def home():
-    return render_template('home.html')
+client = Client(account_sid, auth_token)
 
-app.run(port=5000, debug=True)
+message = client.messages.create(
+    to= my_number, 
+    from_= twilio_number,
+    body="Twilio is good, but its atleast 7X expensive in Australia as compared to USA!")
+
+print("MESSAGE SENT!")
