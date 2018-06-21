46a40926a22215aba85efd33ecaf753ac5baf2b6
ankitsejwal <ankit@sejw.al>
2018-06-22 01:13:37 +1000

    recieving sms through webhook

--- recieve_sms.py
+++ recieve_sms.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
 
 app = Flask(__name__)
@@ -7,7 +7,10 @@ app = Flask(__name__)
 @app.route('/sms', methods=['GET', 'POST'])
 def sms_reply():
     resp = MessagingResponse()
-    resp.message("The robots are coming!! But they are cute.")
+    from_number = request.values.get('From')
+    # from_number = request.form['From']
+    text_message = request.form["Body"]
+    resp.message("-> \nYou texted: " + text_message + ".\nYou texted from: " + str(from_number))
     return str(resp)
 
 if __name__ == '__main__':
